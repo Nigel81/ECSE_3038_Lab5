ae9c3dc4996660c8470f9597f65ba590f223502f
Nigel George <nigelgeorge142@gmail.com>
2025-03-16 23:32:21 -0400

    Updated code

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,65 @@
+
+#include <LiquidCrystal.h>
 #include <Arduino.h>
-#include <HTTPClient.h>
-#include "env.h"
 #include <WiFi.h>
+#include "env.h"
+#include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-void setup(){
-   
-    Serial.begin(115200);
-    lcd.begin(16,2);  // Initialize the LCD
-    lcd.backlight();
-    lcd.print("Hello");
-    if (IS_WOKWI) 
-    WiFi.begin(SSID, PASS, CHANNEL);
-    else 
-    WiFi.begin(SSID, PASS);
 
-    // while (WiFi.status()!= WL_CONNECTED)
-    // {
-    //     delay(500);
-    //     Serial.print("."); //waits for wifi connection to then jump out the while loop
-    // }
+LiquidCrystal lcd(4, 5, 18, 19, 21, 22);
 
-    // Serial.println("Connected to the WiFi Network with IP address: ");
-    // Serial.println(WiFi.localIP());
+void setup() {
 
-    
-    
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+
+  lcd.begin(16, 2); 
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, ESP32!");
+  
+  if (IS_WOKWI) 
+  WiFi.begin(SSID, PASS, CHANNEL);
+  else 
+  WiFi.begin(SSID, PASS);
+
+  while(WiFi.status() != WL_CONNECTED){
+
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Wifi connect. IP address is: ");
+  Serial.println(WiFi.localIP());
+
 }
 
-void loop(){
-    if(WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
+void loop() {
 
-        http.begin(ENDPOINT); //just like localhost8000 used previously
-        http.addHeader("api-key", apiKey);
+  if (WiFi.status()==WL_CONNECTED){
 
-        JsonDocument object;
-        String request_body;
-    
-        object["line_1"] = "message1";  
-        object["line_2"] = "message2";
-            
-        serializeJson(object, request_body);
+    HTTPClient http;
+    http.begin(ENDPOINT);
+    http.addHeader("api-key",(apiKey));
+    int responseCode = http.GET();
 
-        // http.GET(); 
+    Serial.print("HTTP Response: ");
+    Serial.println(responseCode);
 
-        int responseCode = http.GET(); // -1 if request was unsuccessful 
+    if (responseCode == HTTP_CODE_OK){
+      String response = http.getString();
+      Serial.print("HTTP Response: ");
+      Serial.println(response);
 
-        if (responseCode <= 0){
-            Serial.print("An error occurred with this code: ");
-            Serial.println(responseCode);
-            http.end(); //close open connections 
-            return;
-        }
-        Serial.print("HTTP Response Code: ");
-        Serial.println(responseCode);
+      JsonDocument object;
+      DeserializationError error = deserializeJson(object, response);
 
-        String response = http.getString();
-        Serial.println(response);
-        http.end();
+      if(error){
+        Serial.print("Deserialization failed: ");
+        Serial.println(error.c_str());
+      }else {
+        const char* line_1 = object["line_1"];
+        const char* line_2 = object["line_2"];
 
-        DeserializationError error = deserializeJson(object, response);  //response  string to object json 
-
-        if(error){  //error occured
-            Serial.print("Deserialization failed: ");
-            Serial.println(error.c_str());
-            return;
-        }
-
-        String line_1 = object["line1"];
-        String line_2 = object["line2"];
-        
         lcd.setCursor(0, 0);
         lcd.print("Line1: ");
         lcd.print(line_1);
@@ -81,6 +67,107 @@ void loop(){
         lcd.print("Line2: ");
         lcd.print(line_2);
 
-
+        Serial.print("Line 1: ");
+        Serial.println(line_1);
+        Serial.print("Line 2: ");
+        Serial.println(line_2);
+      }
+    } else {
+      Serial.print("HTTP Request Failed: ");
+      Serial.println(responseCode);
     }
+
+    http.end();
+  } else {
+    Serial.println("Wifi Connection Lost");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Wifi Lost");
+  }
+  delay(1000);
+
 }
+// #include <Arduino.h>
+// #include <HTTPClient.h>
+// #include "env.h"
+// #include <WiFi.h>
+// #include <ArduinoJson.h>
+// #include <LiquidCrystal.h>
+// // #include <Wire.h>
+
+// LiquidCrystal lcd(4, 5, 18, 19, 21, 22);;
+// void setup(){
+   
+//     Serial.begin(115200);
+//     lcd.begin(16,2);  // Initialize the LCD
+//     lcd.print("Hello");
+//     if (IS_WOKWI) 
+//     WiFi.begin(SSID, PASS, CHANNEL);
+//     else 
+//     WiFi.begin(SSID, PASS);
+
+//     while (WiFi.status()!= WL_CONNECTED)
+//     {
+//         delay(500);
+//         Serial.print("."); //waits for wifi connection to then jump out the while loop
+//     }
+
+//     Serial.println("Connected to the WiFi Network with IP address: ");
+//     Serial.println(WiFi.localIP());
+
+    
+    
+// }
+
+// void loop(){
+//     if(WiFi.status() == WL_CONNECTED) {
+//         HTTPClient http;
+
+//         http.begin(ENDPOINT); //just like localhost8000 used previously
+//         http.addHeader("api-key", apiKey);
+
+
+//         // http.GET(); 
+
+//         int responseCode = http.GET(); // -1 if request was unsuccessful 
+
+//         if (responseCode <= 0){
+//             Serial.print("An error occurred with this code: ");
+//             Serial.println(responseCode);
+//             http.end(); //close open connections 
+//             return;
+//         }
+//         Serial.print("HTTP Response Code: ");
+//         Serial.println(responseCode);
+
+//         String response = http.getString();
+//         Serial.println(response);
+//         http.end();
+
+//         JsonDocument object;
+//         DeserializationError error = deserializeJson(object, response);  //response  string to object json 
+
+//         if(error){  //error occured
+//             Serial.print("Deserialization failed: ");
+//             Serial.println(error.c_str());
+//             return;
+//         }
+
+//         const char* line_1 = object["line1"];
+//         const char* line_2 = object["line2"];
+        
+//         lcd.setCursor(0, 0);
+//         lcd.print("Line1: ");
+//         lcd.print(line_1);
+//         lcd.setCursor(0, 1);
+//         lcd.print("Line2: ");
+//         lcd.print(line_2);
+//     }
+//     else {
+//         Serial.println("Wifi Connection Lost");
+//         lcd.clear();
+//         lcd.setCursor(0,0);
+//         lcd.print("Wifi Lost");
+//       }
+//       delay(1000);
+// }
